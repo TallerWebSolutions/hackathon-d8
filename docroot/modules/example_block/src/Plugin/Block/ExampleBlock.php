ebfc637794b345f9eedacc575d8bcbce72705669
Luiz Amorim <luiz.simples@gmail.com>
2016-05-18 10:34:48 -0300

    added example block

---
+++ docroot/modules/example_block/src/Plugin/Block/ExampleBlock.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Provides a 'Hello' Block
+ *
+ * @Block(
+ *   id = "example_block",
+ *   admin_label = @Translation("Example Block"),
+ * )
+ */
+
+namespace Drupal\example_block\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+class ExampleBlock  extends BlockBase {
+  public function build() {
+    return array(
+      '#markup' => $this->t('Hello, World!'),
+    );
+  }
+}

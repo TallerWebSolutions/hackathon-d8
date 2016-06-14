b7d740611b17555edae1b10f6149b7bf6d390356
Rafael Caceres <rafael@taller.net.br>
2016-06-14 12:03:56 -0300

    Initial drupal config scripts.

--- server/docker-entrypoint.sh
+++ server/docker-entrypoint.sh
@@ -10,10 +10,10 @@ sudo chown $UID:$UID -R /drupal/src
 
 while ! nc -q 1 drupal-db 3306 </dev/null; do sleep 3; done
 
-if [ ! -f /drupal/src/docroot/sites/default/settings.php ]
+if [ ! -f /drupal/src/docroot/sites/default/settings.local.php ]
 then
-  cp /drupal/src/docroot/sites/default/default.settings.php /drupal/src/docroot/sites/default/settings.php
-  sudo chmod 777 -Rf /drupal/src/docroot/sites/default/settings.php
+  cp /drupal/server/settings.local.php /drupal/src/docroot/sites/default/settings.local.php
+  sudo chmod 777 -Rf /drupal/src/docroot/sites/default/settings.local.php
 
   mkdir -p /drupal/src/docroot/sites/default/files
 
